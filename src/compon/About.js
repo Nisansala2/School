ba0bd9115bdbca7167d8866e541821c0414dd74a
Nisansala2 <nisansalagamchchige@gmail.com>
2024-06-17 02:11:40 +0530

    add section in to the page

--- src/compon/About.js
+++ src/compon/About.js
@@ -4,18 +4,7 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <div className="about-container">
-      <div className='about-image'>
-     <img src='/images/amp.jpg' alt='school'  style={{ maxWidth: '100%', height: '50%' }}/>
-     </div >
-     <div className='text-about'>
-     <h2>Vision</h2>
-     <p>To endow the society with a well-rounded child<br/> who can ovrcome challenges</p>
-     <br/>
-     <h2>Mission</h2>
-      <p>To give birth to a child equipped with knoweldege,<br/> qualities and skill suitablr for the future worldS</p>      
-      </div>
-    </div>
+      
      
     </div>
   );
