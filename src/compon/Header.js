d578c33a84f682953c0a904c0b87b4ba0caa44d6
Nisansala2 <nisansalagamchchige@gmail.com>
2024-06-16 19:33:54 +0530

    change header part adding css

--- src/compon/Header.js
+++ src/compon/Header.js
@@ -3,21 +3,28 @@ import React from 'react';
 
 const Header = () => {
   return (
+    
+    
     <header>
-      <h3 className='top'>The Place where life is built without bring limited to knoweldge</h3>
-      <img src='/images/logo.jpg'alt="School Logo" style={{ height: '50px' }} />
-      <h1>Athapattukanda Collage</h1>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/acadamic">Acadamic</a></li>
-          <li><a href="/Administration">Administration</a></li>
-          <li><a href="/contact">Contact</a></li>
-          
-        </ul>
+        <h3 className='top' >The Place where life is built without bring limited to knoweldge</h3>
+          <div className='header'>
+            <div className='top-image'>
+        <img src='/images/logo.jpg'alt="School Logo" style={{ height: '100px' } } className='logo'/>
+        </div>
+        <h1 className='top-name'>Athapattukanda Collage</h1>
+        
+       <nav className='nav'>
+        <ul className='nav-list'>
+          <li className='nav-item'><a href="/">Home</a></li>
+          <li className='nav-item'><a href="/about">About</a></li>
+          <li className='nav-item'><a href="/acadamic">Acadamic</a></li>
+          <li className='nav-item'><a href="/Administration">Administration</a></li>
+          <li className='nav-item'><a href="/contact">Contact</a></li>
+          </ul>
       </nav>
+      </div>
     </header>
+  
   );
 }
 
